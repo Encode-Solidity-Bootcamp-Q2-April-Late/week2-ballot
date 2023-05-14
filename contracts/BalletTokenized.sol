37837f73137056f66bb079555c241344fdb87201
KS <kitsidhu@gmail.com>
2023-05-14 10:58:20 -0400

    first commit

---
+++ contracts/BalletTokenized.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+// The ERC20 interface from the token contract.
+interface IMyVoteToken{
+    function getPastVotes(address, uint256) external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract Ballot {
+    // Voter struct will keep track of whether a voter has voted,
+    // the weight of their vote, which proposal they voted for,
+    // and if they have delegated their vote to someone else.
+    struct Voter {
+        uint weight; 
+        bool voted;  
+        address delegate; 
+        uint vote;   
+    }
+
+    // Proposal struct will keep track of each proposal's name 
+    // and the number of voteCount it has received.
+    struct Proposal {
+        bytes32 name;   
+        uint voteCount; 
+        uint256 endTime; // added endTime to set a duration for each proposal's voting period.
+    }
+
+    // tokenContract will keep track of the ERC20 token used for voting.
+    IMyVoteToken public tokenContract;    
+    Proposal[] public proposals;
+    uint256 public targetBlockNumber;
+
+    // Mapping to track how much voting power has been spent by each address.
+    mapping(address => uint256) public  votingPowerSpent;
+
+    // Added a mapping to track a voter's information.
+    mapping(address => Voter) public voters;
+
+    // The constructor will initiate the ballot with a list of proposal names, 
+    // the ERC20 token contract address, and the target block number.
+    constructor(bytes32[] memory proposalNames, address _tokenContract, uint256 _targetBlockNumber) {
+        tokenContract = IMyVoteToken(_tokenContract);
+        targetBlockNumber = _targetBlockNumber;
+        for (uint i = 0; i < proposalNames.length; i++) {
+            // Added an endTime to each proposal to indicate when voting period ends.
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0,
+                endTime: block.timestamp + _targetBlockNumber // Voting period will last until targetBlockNumber seconds have passed since contract deployment.
+            }));
+        }
+    }
+
+    // The vote function allows a voter to vote on a proposal.
+    // The voter's token balance at the time of voting is used as the weight of their vote.
+    function vote(uint proposal, uint amount) external {
+        require(tokenContract.getPastVotes(msg.sender,targetBlockNumber) >= amount);
+        require(tokenContract.balanceOf(msg.sender) >= amount, "Not enough tokens to vote with this weight");
+        require(!voters[msg.sender].voted, "You have already voted");
+        require(proposals[proposal].endTime > block.timestamp, "Voting period for this proposal has ended");
+
+        voters[msg.sender].weight = amount;
+        voters[msg.sender].voted = true;
+        voters[msg.sender].vote = proposal;
+
+        proposals[proposal].voteCount += amount;
+        votingPowerSpent[msg.sender] += amount;
+    }
+
+    // winningProposal function will return the index of the proposal with the most votes.
+    function winningProposal() public view returns (uint winningProposal_) {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                            winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    // winnerName function will return the name of the proposal with the most votes.
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
+    
---
+++ tests/TokenizedBallot.ts
@@ -0,0 +1,98 @@
+import { Ballot, Ballot__factory, MyERC20Votes, MyERC20Votes__factory } from "../typechain-types";
+
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+const MINT_AMOUNT = ethers.utils.parseEther("1000");
+const TRANSFER_AMOUNT = ethers.utils.parseEther("100");
+const PROPOSALS = ["Prop1", "Prop2", "Prop3"];
+const VOTING_PERIOD = 150;
+
+function convertStringArrayToBytes32(array: string[]) {
+    return array.map(ethers.utils.formatBytes32String);
+}
+
+describe("Tokenized Ballot Test", function () {
+    let votingTokenContract: MyERC20Votes;
+    let acc1: SignerWithAddress;
+    let acc2: SignerWithAddress;
+    let acc3: SignerWithAddress;
+    let ballotContract: Ballot;
+    let votingTokenContractFactory: MyERC20Votes__factory;
+    let ballotContractFactory: Ballot__factory;
+
+    beforeEach(async function () {
+        const accounts = await ethers.getSigners();
+        [, acc1, acc2, acc3] = accounts;
+
+        votingTokenContractFactory = await ethers.getContractFactory("MyERC20Votes") as MyERC20Votes__factory;
+        votingTokenContract = await votingTokenContractFactory.deploy();
+        await votingTokenContract.deployed();
+
+        const MINTER_ROLE = await votingTokenContract.MINTER_ROLE();
+    await votingTokenContract.grantRole(MINTER_ROLE, acc1.address);
+
+        ballotContractFactory = await ethers.getContractFactory("Ballot") as Ballot__factory;
+        ballotContract = await ballotContractFactory.deploy(
+            convertStringArrayToBytes32(PROPOSALS),
+            votingTokenContract.address,
+            VOTING_PERIOD
+        );
+        await ballotContract.deployed();
+    });
+
+    describe("when the contract is deployed", function () {
+        it("has the provided proposals", async function () {
+            for (let index = 0; index < PROPOSALS.length; index++) {
+                const proposal = await ballotContract.proposals(index);
+                expect(ethers.utils.parseBytes32String(proposal.name)).to.eq(PROPOSALS[index]);
+            };
+        });
+
+        it("has zero vote count", async function () {
+            for (let index = 0; index < PROPOSALS.length; index++) {
+                const proposal = await ballotContract.proposals(index);
+                expect(proposal.voteCount).to.eq(0);
+            }
+        });
+    });
+
+    describe("Token and voting functionality", function () {
+        it("Should mint tokens to acc1", async function () {
+            await votingTokenContract.connect(acc1).mint(acc1.address, MINT_AMOUNT);
+            const acc1Balance = await votingTokenContract.balanceOf(acc1.address);
+            console.log("Acc1 balance after minting: ", acc1Balance.toString());
+            expect(acc1Balance).to.equal(MINT_AMOUNT);
+        });
+    
+        it("Should transfer tokens from acc1 to acc2 and acc3", async function () {
+            await votingTokenContract.connect(acc1).transfer(acc2.address, TRANSFER_AMOUNT);
+            await votingTokenContract.connect(acc1).transfer(acc3.address, TRANSFER_AMOUNT);
+            const acc2Balance = await votingTokenContract.balanceOf(acc2.address);
+            const acc3Balance = await votingTokenContract.balanceOf(acc3.address);
+            console.log("Acc2 balance after transfer: ", acc2Balance.toString());
+            console.log("Acc3 balance after transfer: ", acc3Balance.toString());
+            expect(acc2Balance).to.equal(TRANSFER_AMOUNT);
+            expect(acc3Balance).to.equal(TRANSFER_AMOUNT);
+        });
+    
+        // it("Should allow each account to vote on a proposal", async function () {
+        //     await ballotContract.connect(acc2).vote(0, ethers.utils.parseEther("10")); // acc2 votes 10 tokens on proposal 0
+        //     await ballotContract.connect(acc3).vote(1, ethers.utils.parseEther("10")); // acc3 votes 10 tokens on proposal 1
+        //     const proposal0 = await ballotContract.proposals(0);
+        //     const proposal1 = await ballotContract.proposals(1);
+        //     console.log("Proposal 0 vote count after voting: ", proposal0.voteCount.toString());
+        //     console.log("Proposal 1 vote count after voting: ", proposal1.voteCount.toString());
+        // });
+    
+        // it("Should correctly tally votes on each proposal", async function () {
+        //     const proposal0 = await ballotContract.proposals(0);
+        //     const proposal1 = await ballotContract.proposals(1);
+        //     console.log("Final vote count for proposal 0: ", proposal0.voteCount.toString());
+        //     console.log("Final vote count for proposal 1: ", proposal1.voteCount.toString());
+        //     expect(proposal0.voteCount).to.equal(ethers.utils.parseEther("20")); // acc1 and acc2 both voted 10 tokens on proposal 0
+        //     expect(proposal1.voteCount).to.equal(ethers.utils.parseEther("10")); // acc3 voted 10 tokens on proposal 1
+        // });
+    });
+});
